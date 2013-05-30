e0841c717430e41b1b8d1d1870acd67f60b76617
Andres F. Mosquera <moscoquera@gmail.com>
2013-05-30 17:39:07 -0500

    solucionado el problema C
    no es bueno hacer un sort en un array de int

--- CodeForces/codeforces186/C.java
+++ CodeForces/codeforces186/C.java
@@ -2,35 +2,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StreamTokenizer;
 import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author AndresFelipe
  */
 public class C {
-    static long contador[];
+
+    static Integer contador[];
     static int n;
-    public static void main(String Args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.valueOf(br.readLine());
+
+    public static void main(String Args[]) throws IOException {
+        StreamTokenizer br = new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+        br.nextToken();
+        int num = (int)br.nval;
         double base = Math.log(4);
-        n= (int)(Math.log(num)/base);
-        contador = new long[num];
-        String numeros = br.readLine();
-                for (int i=0;i<num;i++){
-           // int d = Integer.valueOf(st.nextToken());
-            
+        n = (int) (Math.log(num) / base);
+        contador = new Integer[num];
+        for (int i = 0; i < num; i++) {
+            br.nextToken();
+            contador[i]=(int)br.nval;
         }
+        Arrays.sort(contador);
+        long salida=0;
+        int lim=num-(int)Math.pow(4, n-1);
+        long mult=1;
+        for (int i=0;i<num;i++){
+            if (i>=lim){
+                n--;
+                lim=num-(int)Math.pow(4, n-1);
+                mult++;
+            }
+            salida+=(contador[i]*mult);
+        }
+        System.out.println(Long.toString(salida));
     }
-    
-   
-    
 }
+
